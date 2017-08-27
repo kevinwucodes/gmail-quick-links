98b641b199d8f41c9dfee10e4c477d5e31d0a7af
Kevin Wu <kevin.wu@gmail.com>
2017-08-27 13:54:57 -0700

    separate dev and dist

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+// the path(s) that should be cleaned
+let pathsToClean = [
+  'dev',
+  'dist'
+]
+
+const config = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(pathsToClean),
+
+    // new webpack.optimize.UglifyJsPlugin()
+
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
+
+module.exports = config
---
+++ webpack.dev.js
@@ -0,0 +1,27 @@
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const path = require('path')
+
+module.exports = merge(common, {
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dev'),
+    filename: 'dev.gmailquicklinks.bundle.js'
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: './manifest.dev.json',
+        to: './manifest.json'
+      },{
+        from: './src/assets/icon16.png',
+        to: './assets/icon16.png'
+      },{
+        from: './src/assets/icon48.png',
+        to: './assets/icon48.png'
+      }
+    ]),
+  ]
+})
