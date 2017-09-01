5abb312b44d2d5e12abacc0956710e1701c13947
Kevin Wu <kevin.wu@gmail.com>
2017-09-01 10:42:38 -0700

    css style changes

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,11 +3,21 @@ import React from 'react'
 const Link = ({ name, urlHash, onClickLink, onDelete }) => {
   return (
     <div className="pm">
-      <a className="po" href={urlHash}>
+      <a
+        style={{
+          color: getComputedStyle(document.getElementsByClassName("pU")[0]).color
+        }}
+        className="po"
+        href={urlHash}>
         {name}
       </a>
       <span
         className="pl"
+        style={{
+          float: "right",
+          cursor: "pointer",
+          color: getComputedStyle(document.getElementsByClassName("pU")[0]).color
+        }}
         onClick={event => onDelete(name)}>
         x
       </span>
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import Link from './Link'
 
+const style = {
+  small: {
+    fontSize: "80%"
+  },
+  quick: {
+    cursor: "auto",
+    position: "relative",
+    overflow: "hidden",
+    verticalAlign: "middle",
+    outline: "none",
+    fontSize: "100%"
+  },
+  list: {
+    textAlign: "left",
+    fontSize: "100%",
+    background: "none",
+    margin: 0,
+    textDecoration: "none",
+    paddingLeft: 30
+  }
+}
+
 const renderList = linkList => onDelete => {
   if (Object.keys(linkList).length === 0) {
     return (
-      <div>nothing to list, click add to add one</div>
+      <div className="pU" style={{fontSize:"100%", textAlign:"left", cursor: "auto"}}>
+        nothing to list: To add one, enter a gmail search and click "Add Quick Link" to create a quick list
+      </div>
     )
   } else {
     return (
@@ -28,18 +52,19 @@ const LinkList = ({ linkList = [], onAdd, onDelete }) => {
     <div className="ApVoH">
 
       <div className="r">
-        <div className="py pv">
+        <div className="pv" style={style.quick}>
           <h2 className="pw">Quick Links</h2>
         </div>
       </div>
 
       <div id="listContainer">
         <div className="pt">
-          <div className="pn">
+          <div className="pn" style={style.list}>
             { renderList(linkList)(onDelete) }
           </div>
           <div
             className="QOxrP pU"
+            style={{fontSize:"100%"}}
             onClick={event => onAdd(event)}>
             Add Quick Link
           </div>
