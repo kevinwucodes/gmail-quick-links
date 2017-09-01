540043885e79890e44af19902e26547a09c1738a
Kevin Wu <kevin.wu@gmail.com>
2017-09-01 11:01:18 -0700

    no longer need document.onreadystatechange due to default loading

--- src/index.js
+++ src/index.js
@@ -12,27 +12,20 @@ const beginReact = () => {
   )
 }
 
-const start = () => {
-  if (document.readyState === "complete") {
-    console.info('Loaded Gmail Quick Links')
+// there are cases where the gmail left nav panel isn't loaded yet,
+// so we need to check for it until it exists before we can proceed
+const checkDomElementExist = setInterval(() => {
+  if (getGmailLocationToInject()) {
+    clearInterval(checkDomElementExist)
 
-    // there are cases where the gmail left nav panel isn't loaded yet,
-    // so we need to check for it until it exists before we can proceed
-    var checkDomElementExist = setInterval(() => {
-      if (getGmailLocationToInject()) {
-        clearInterval(checkDomElementExist)
+    // create a div for our react container to be injected
+    const gmailQuickLinksContainer = document.createElement('div')
+    gmailQuickLinksContainer.id = "gmailQuickLinksContainer"
+    getGmailLocationToInject().append(gmailQuickLinksContainer)
 
-        // create a div for our react container to be injected
-        const gmailQuickLinksContainer = document.createElement('div')
-        gmailQuickLinksContainer.id = "gmailQuickLinksContainer"
-        getGmailLocationToInject().append(gmailQuickLinksContainer)
+    console.log('Loaded Gmail Quick Links')
 
-        //load react
-        beginReact()
-      }
-    }, 200)
+    //load react
+    beginReact()
   }
-}
-
-// run when page is ready
-document.onreadystatechange = start
+}, 200)
