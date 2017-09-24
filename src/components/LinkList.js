78f77c53949674614d910be36e81e62433e1236d
Kevin Wu <kevin.wu@gmail.com>
2017-09-24 16:12:30 -0700

    the "nothing to list" text should not be styled like a link

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -31,7 +31,7 @@ const renderList = linkList => accountList => onDelete => onClickGlobeCircle => 
 
   if (Object.keys(linkList).length === 0 && Object.keys(accountList).length === 0) {
     return (
-      <div className="pU" style={{fontSize:"100%", textAlign:"left", cursor: "auto"}}>
+      <div className="n0" style={{fontSize:"100%", textAlign:"left", cursor: "auto"}}>
         nothing to list: To add one, enter a gmail search and click "Add Quick Link" to create a quick list
       </div>
     )
