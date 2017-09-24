9cc77bf9b39ee900c76f934ca3f607382434d37a
Kevin Wu <kevin.wu@gmail.com>
2017-09-24 16:22:11 -0700

    version bump to 0.2.3

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const GMAIL_QUICK_LINKS_NAME = {
   name: "Gmail Quick Links",
   divId: "gmailQuickLinks",
-  version: "0.2.2"
+  version: "0.2.3"
 }
 
 // this should come out of chrome.storage apis
