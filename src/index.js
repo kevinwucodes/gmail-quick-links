8a37923ae6e8da62bc2da4d1dfbe2dae44f9d70f
Kevin Wu <kevin.wu@gmail.com>
2018-12-11 19:11:59 -0800

    remove unneeded material ui check

--- src/index.js
+++ src/index.js
@@ -26,34 +26,24 @@ const injectReact = location => {
   if (location === 'widget') {
     widgetInsidePanel().append(gmailQuickLinksContainer)
   } else {
-    if (isMaterialUi()) {
-      labelControlsContainer().insertAdjacentElement(
-        'beforebegin',
-        gmailQuickLinksContainer
-      )
-    } else {
-      getGmailLocationToInject().append(gmailQuickLinksContainer)
-    }
+    labelControlsContainer().insertAdjacentElement(
+      'beforebegin',
+      gmailQuickLinksContainer
+    )
   }
 
   console.log('Loaded Gmail Quick Links')
 
   //TODO: what is the person isn't signed in?  Does this crash extension?
-  const currentAccountName = isMaterialUi()
-    ? //new gmail ui
-      (
-        document.querySelectorAll('a[class="gb_b gb_eb gb_R"]')[0] ||
-        document.querySelectorAll('a[class="gb_b gb_db gb_R"]')[0] ||
-        document.querySelectorAll('a[class="gb_b gb_fb gb_R"]')[0] ||
-        document.querySelectorAll('a[class="gb_b gb_gb gb_R"]')[0] ||
-        document.querySelectorAll('a[class="gb_b gb_hb gb_R"]')[0] ||
-        document.querySelectorAll('a[class="gb_b gb_ib gb_R"]')[0] ||
-        document.querySelectorAll('a[class="gb_b gb_jb gb_R"]')[0]
-      ).attributes['aria-label'].nodeValue.match(/\(([^)]+)\)/)[1]
-    : //old gmail ui
-      document
-        .querySelectorAll('a[href*="accounts.google.com"]')[0]
-        .title.match(/\(([^)]+)\)/)[1]
+  const currentAccountName = (
+    document.querySelectorAll('a[class="gb_b gb_eb gb_R"]')[0] ||
+    document.querySelectorAll('a[class="gb_b gb_db gb_R"]')[0] ||
+    document.querySelectorAll('a[class="gb_b gb_fb gb_R"]')[0] ||
+    document.querySelectorAll('a[class="gb_b gb_gb gb_R"]')[0] ||
+    document.querySelectorAll('a[class="gb_b gb_hb gb_R"]')[0] ||
+    document.querySelectorAll('a[class="gb_b gb_ib gb_R"]')[0] ||
+    document.querySelectorAll('a[class="gb_b gb_jb gb_R"]')[0]
+  ).attributes['aria-label'].nodeValue.match(/\(([^)]+)\)/)[1]
 
   //load react
   beginReact(currentAccountName)(location)
@@ -77,16 +67,6 @@ const checkWidgetPanel = untilStop => {
     }
   }, 500)
 }
-
-//is this the new gmail?  do we have an image showing the new gmail ui logo?
-const isMaterialUi = () =>
-  document.getElementsByClassName('gb_Wa')[1] ||
-  document.getElementsByClassName('gb_Va')[1] ||
-  document.getElementsByClassName('gb_Xa')[1] ||
-  document.getElementsByClassName('gb_Ya')[1] ||
-  document.getElementsByClassName('gb_Za')[1]
-    ? true
-    : false
 
 // there are cases where the gmail left nav panel isn't loaded yet,
 // so we need to check for it until it exists before we can proceed
