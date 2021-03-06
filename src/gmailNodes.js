b7857bb90d5a7fa620f4711fea62a2c155922926
Kevin Wu <kevin.wu@gmail.com>
2021-03-06 10:44:13 -0800

    add console debug information during gmail nodes detection

--- src/gmailNodes.js
+++ src/gmailNodes.js
@@ -1,30 +1,64 @@
 const getGmailLocationToInject = () => {
+  const node = document.querySelector('div.wT')
+  const isFound = node ? true : false
+  console.log(`GQL:getGmailLocationToInject = ${isFound.toString()}`)
   // where we want to put the search quick links
   // this has be be defined after the page loads and becomes ready
-  return document.querySelector('div.wT')
+  return node
 }
 
-const gmailAccountName = () =>
-  Array.from(document.querySelectorAll('a[aria-label]'))
+const gmailAccountName = () => {
+  const node = Array.from(document.querySelectorAll('a[aria-label]'))
     .map(n => n.attributes['aria-label'].nodeValue)
     .find(t => /\(.+@.+\)/.test(t))
     .match(/\((.+@.+)\)/)[1]
+  const isFound = node ? true : false
+  console.log(`GQL:gmailAccountName = ${isFound.toString()} "${node}"`)
+  return node
+}
 
 //inside gmail controls container - contains labels such as inbox/starred/drafts/etc
-const labelControlsContainer = () =>
-  document.getElementsByClassName('ajl aib aZ6')[0]
+const labelControlsContainer = () => {
+  const node = document.getElementsByClassName('ajl aib aZ6')[0]
+  const isFound = node ? true : false
+  console.log(`GQL:labelControlsContainer = ${isFound.toString()}`)
+  return node
+}
 
-const widgetMainPanel = () => document.querySelector('div.akc.aZ6')
+const widgetMainPanel = () => {
+  const node = document.querySelector('div.akc.aZ6')
+  const isFound = node ? true : false
+  console.log(`GQL:widgetMainPanel = ${isFound.toString()}`)  
+  return node
+}
 
-const widgetInsidePanel = () => document.querySelector('div.T0.pp.saH2Ef')
+const widgetInsidePanel = () => {
+  const node = document.querySelector('div.T0.pp.saH2Ef')
+  const isFound = node ? true : false
+  console.log(`GQL:widgetInsidePanel = ${isFound.toString()}`)  
+  return node
+}
 
-const gmailSideBar = () => document.querySelector('div.nH.oy8Mbf.nn.aeN')
+const gmailSideBar = () => {
+  const node = document.querySelector('div.nH.oy8Mbf.nn.aeN')
+  const isFound = node ? true : false
+  console.log(`GQL:gmailSideBar = ${isFound.toString()}`)  
+  return node
+}
 
-const gmailHoverSideBar = () =>
-  document.querySelector('div.nH.oy8Mbf.nn.aeN.bhZ.bym')
+const gmailHoverSideBar = () => {
+  const node = document.querySelector('div.nH.oy8Mbf.nn.aeN.bhZ.bym')
+  const isFound = node ? true : false
+  console.log(`GQL:gmailHoverSideBar = ${isFound.toString()}`)  
+  return node
+}
 
-const quickLinksContainer = () =>
-  document.querySelector('#gmailQuickLinksContainer')
+const quickLinksContainer = () => {
+  const node = document.querySelector('#gmailQuickLinksContainer')
+  const isFound = node ? true : false
+  console.log(`GQL:quickLinksContainer = ${isFound.toString()}`)  
+  return node
+}
 
 export {
   gmailSideBar,
